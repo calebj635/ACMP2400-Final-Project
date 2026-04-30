4cf7ad2d42405873d08489cdb2e11eb06a2f5e3c
CJ <130240579+calebj635@users.noreply.github.com>
2026-04-29 20:51:55 -0500

    Update entrypoint.sh

--- .github/actions/terraform/entrypoint.sh
+++ .github/actions/terraform/entrypoint.sh
@@ -8,9 +8,18 @@ export ARM_SUBSCRIPTION_ID=${INPUT_ARM_SUBSCRIPTION_ID}
 export ARM_TENANT_ID=${INPUT_ARM_TENANT_ID}
 export STATE_KEY=${INPUT_STATE_KEY}
 export TF_STAGE=${INPUT_TF_STAGE}
+export DJANGO_SECRET_KEY_PROD=${INPUT_DJANGO_SECRET_KEY_PROD}
 
+#STAGE 1
+#Configure provider, remote state store, and azure container registry
 if [[ "$TF_STAGE" == "stage1" ]]; then
   terraform -chdir=${INPUT_TF_STAGE} init -backend-config="key=${INPUT_STATE_KEY}.tfstate"
   terraform -chdir=${INPUT_TF_STAGE} plan -out=${INPUT_TF_STAGE}.tfplan
   terraform -chdir=${INPUT_TF_STAGE} apply ${INPUT_TF_STAGE}.tfplan
+#STAGE 2
+#Configure azure container instance that will run in cloud. We must autoapprove bc of the non-interactivity.
+elif [[ "$TF_STAGE" == "stage2" ]]; then
+  terraform -chdir=${INPUT_TF_STAGE} init -beckend-config="key=${INPUT_STATE_KEY}.tfstate"
+  terraform -chdir=${INPUT_TF_STAGE} apply -auto-approve -var="ARM_CLIENT_ID=${INPUT_ARM_CLIENT_ID}" -var="ARM_CLIENT_SECRET=${INPUT_ARM_CLIENT_SECRET}" -var="DJANGO_SECRET_KEY_PROD=${DJANGO_SECRET_KEY_PROD}"
+  
 fi
